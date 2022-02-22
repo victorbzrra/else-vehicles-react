5b9a859c65d02aa9487b90feb8ffc9c5aaef0d99
Victor Bezerra <victorbzrra@alu.ufc.br>
2022-02-22 11:46:03 -0300

    :construction: feat: construindo tela de administração.

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,5 +1,50 @@
+import { Col, Row, Input, Button, Table } from "antd";
+import { PlusOutlined } from '@ant-design/icons';
+
 export function Admin() {
+  const { Search } = Input;
+
+  const columns = [
+    {
+      title: 'Modelo'
+    },
+    {
+      title: 'Marca'
+    },
+    {
+      title: 'Preço'
+    },
+    {
+      title: 'Km'
+    },
+    {
+      title: 'Data'
+    },
+    {
+      title: 'Editar'
+    },
+    {
+      title: 'Excluir'
+    }
+  ];
+
   return (
-    <h1>Administração</h1>
-  )
+    <>
+      <Row justify="space-around" style={{ marginBottom: 50, minWidth: 550 }}>
+        <Col xl={6} lg={6}>
+          <Search placeholder="Modelo, Marca, Ano, Cor" enterButton />
+        </Col>
+        <Col>
+          <Button type="primary" icon={<PlusOutlined />}>
+            Nova Oferta
+          </Button>
+        </Col>
+      </Row>
+      <Row justify="center" style={{minWidth: 550}}>
+        <Col md={16} xl={16} lg={16}>
+          <Table columns={columns} />
+        </Col>
+      </Row>
+    </>
+  );
 }
